9cc5f1dd1ab979254f28c794feaae7f20644c454
me-2012 <aniarmen56@gmail.com>
2025-05-21 17:19:42 -0700

    added rounding

--- pi.py
+++ pi.py
@@ -8,31 +8,62 @@ else:
 
 notfound = input("empty variable (c, d, r, a): ")
 
+rounding = input("rounding (none for none, 1 for tenths, place -1 for tens place ect): ")
+if rounding == 'none':
+    roundingCheck = False
+else:
+    roundingCheck = True
+    rounding = int(rounding)
+
 if notfound == 'd': #diameter not found
     if input('area or circumference (type "a" or "c"): ') == 'a':
         a = float(input("a: "))
-        print(2 * math.sqrt(a / pi))
+        if roundingCheck:
+            print(round(2 * math.sqrt(a / pi), rounding))
+        else:
+            print(2 * math.sqrt(a / pi))
     else:
         c = float(input("c: "))
-        print(c / pi)
+        if roundingCheck:
+            print(round(c / pi, rounding))
+        else:
+            print(c / pi)
 elif notfound == 'a': #area not found
     if input("d or r: ") == 'd':
         d = float(input("d: "))
-        print((d / 2) ** 2 * pi)
+        if roundingCheck:
+            print(round((d / 2) ** 2 * pi, rounding))
+        else:
+            print((d / 2) ** 2 * pi)
     else:
         r = float(input("r: "))
-        print(r ** 2 * pi)
+        if roundingCheck:
+            print(round(r ** 2 * pi, rounding))
+        else:
+            print(r ** 2 * pi)
 elif notfound == 'c': #circumfrence not found
     if input("d or r: ") == 'd':
         d = float(input("d: "))
-        print(d * pi)
+        if roundingCheck:
+            print(round(d * pi, rounding))
+        else:
+            print(d * pi)
     else:
         r = float(input("r: "))
-        print(2 * r * pi)
+        if roundingCheck:
+            print(round(2 * r * pi, rounding))
+        else:
+            print(2 * r * pi)
 elif notfound == 'r': #radius not found
     if input("c or a: ") == 'c':
         c = float(input("c: "))
-        print((c/pi)/2)
+        if roundingCheck:
+            print(round((c/pi)/2, rounding))
+        else:
+            print((c/pi)/2)
     else:
         a = float(input("a: "))
-        print(math.sqrt(a / pi))
+        if roundingCheck:
+            print(round(math.sqrt(a / pi), rounding))
+        else:
+            print(math.sqrt(a / pi))
